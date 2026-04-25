96c17f4f3b687be954d5ee98a262a989a2d9ea6b
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 08:27:48 +0600

    add

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,11 +1,15 @@
 import LeftSide from '@/component/LeftSide';
+import RightSide from '@/component/RightSide';
 import React from 'react';
 
 const page = () => {
   return (
-    <div>
+    <div className='max-w-[90%] mx-auto grid grid-cols-5'>
       <LeftSide></LeftSide>
-      
+      <div className='col-span-3'>
+
+      </div>
+      <RightSide></RightSide>
     </div>
   );
 };
