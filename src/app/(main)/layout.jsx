a3d552c3e6e066ca6d3a9b238033143810988a06
umayer10-AI <mdumayer10@gmail.com>
2026-04-25 07:51:57 +0600

    add

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,3 +1,4 @@
+import BreakingNews from '@/component/BreakingNews';
 import Header from '@/component/Header';
 import Navbar from '@/component/Navbar';
 import React from 'react';
@@ -6,6 +7,7 @@ const layout = ({children}) => {
     return (
         <div>
             <Header></Header>
+            <BreakingNews></BreakingNews>
             <Navbar></Navbar>
             {children}
         </div>
--- src/component/BreakingNews.jsx
+++ src/component/BreakingNews.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
+
+const news = [
+  {
+    _id: "1",
+    title: "Breaking News: Major Event Unfolds in the City",
+  },
+  {
+    _id: "2",
+    title: "Breaking News: New Policy Announced by the Government",
+  },
+  {
+    _id: "3",
+    title: "Breaking News: Sports Team Wins Championship",
+  },
+];
+
 
 const BreakingNews = () => {
     return (
-        <div>
-            
+        <div className='flex items-center gap-3 max-w-[80%] mx-auto bg-gray-100 py-3 px-2 rounded-lg'>
+            <button className='btn btn-error text-white'>Latest</button>
+            <Marquee pauseOnHover={true}>
+                <div className='flex gap-6'>
+                    {
+                        news.map(v => (
+                            <h2 key={v._id} className='font-semibold'>{v.title}</h2>
+                        ))
+                    }
+                </div>
+            </Marquee>
         </div>
     );
 };
